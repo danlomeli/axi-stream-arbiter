f38e088b141c19e2fd3205ab5deed465d35da739
Daniel Lomeli <dan.lomeli@gmail.com>
2019-02-18 17:01:23 -0800

    Not there with the testbench but close

--- hdl/arbiter.v
+++ hdl/arbiter.v
@@ -7,36 +7,31 @@ module arbiter
 	input  wire          axis_aresetn,
 	// Ports of Axi Slave Bus Interface S0a_AXIS
 	input  wire [31 : 0] s0a_axis_tdata,
-	input  wire  		 s0a_axis_tvalid,                             //req
-	output wire  		 s0a_axis_tready,                             //ack
+	input  wire  		 s0a_axis_tvalid,                             
+	output wire  		 s0a_axis_tready,
 	input  wire  		 s0a_axis_tlast,
 	// Ports of Axi Slave Bus Interface S0b_AXIS
 	input  wire [31 : 0] s0b_axis_tdata,
-	input  wire          s0b_axis_tvalid,                             //req
-	output wire          s0b_axis_tready,                             //ack
+	input  wire          s0b_axis_tvalid,                             
+	output wire          s0b_axis_tready,                           
 	input  wire          s0b_axis_tlast,
 	// Ports of Axi Master Bus Interface M0k_AXIS
-	output wire [31 : 0] m0k_axis_tdata,  //done
-	output wire          m0k_axis_tvalid, //done                            
+	output wire [31 : 0] m0k_axis_tdata,  
+	output wire          m0k_axis_tvalid,                            
 	input wire           m0k_axis_tready,                              
 	output wire          m0k_axis_tlast
 );
 
 
-wire [31:0] dat0_nxt;
-reg  [31:0] dat0;
-wire [31:0] dat1_nxt;
-reg  [31:0] dat1;
-
-wire en0, sel0, en1, sel1;
-// Targets
-wire ack0;
-wire req0;
-// Initiators
-wire ack1;
-reg req1;
-// 
+wire [31:0] dat0_nxt, dat1_nxt;
 wire dat0_tlast, dat0_en, dat1_tlast, dat1_en;
+reg  [31:0] dat0, dat1;
+
+wire en, en1, sel1;
+// Targets
+wire ack0, req0;
+// Initiators
+wire ack1, req1;
 
 elbuf u_elbuf0(
 	.clk      (axis_aclk       ),
@@ -62,13 +57,11 @@ elbuf u_elbuf1(
     .s0_ready (s0b_axis_tready ),
     // Initiators
     .m0_data ({dat1_tlast, dat1_nxt}),
-    .m0_valid (),
-    .m0_ready ()
+    .m0_valid (req1),
+    .m0_ready (ack1)
 );
 
-// assign dat1_en = req1 & ack1;
-assign dat1_en = 0;
-
+assign dat1_en = req1 & ack1;
 
 parameter S0 = 2'b0_0;
 parameter S1 = 2'b1_0;
@@ -81,8 +74,8 @@ always @(posedge axis_aclk or negedge axis_aresetn) begin
 end
 
 //  state   d0  d1  en0     en1     en    sel
-//  0       x   x   s_vld   0       0     0       0_0
-//  1       0   x   0       s_vld   1     0       1_0
+//  0       x   x   1       0       0     0       0_0
+//  1       1   0   0       1       1     0       1_0
 //  2       0   1   0       0       1     1       1_1
 
 always @*
@@ -90,26 +83,40 @@ always @*
         {S0, 4'b??1?} : state_nxt = S1;
         {S0, 4'b???1} : state_nxt = S2;
         {S1, 4'b1?1?} : state_nxt = S0;
-        {S1, 4'b?1?1} : state_nxt = S0;
+        {S2, 4'b?1?1} : state_nxt = S0;
         default       state_nxt = state;
     endcase
 
 assign sel1 = state[0];
 assign en1 = state[1];
 
-assign dat0_nxt = (sel0) ? dat0_nxt : dat1_nxt;
+reg ireq, ilast, ack;
+wire slast, tack, iack, treq;
+wire [31:0] tdata;
+reg [31:0] idata;
 
-assign en0 = req0 & ack0;
-assign ack0 = ~req1 | ack1;
+// Muxs
+assign tdata = (sel1) ? dat1_nxt : dat0_nxt;
+assign slast = (sel1) ? dat1_tlast : dat0_tlast;
+assign treq = (sel1) ? req1 : req0;
+
+// Demux
+always @(posedge axis_aclk or negedge axis_aresetn) if (~axis_aresetn) ack <= 1'b0; else if (state_nxt == S0) ack <= ~ack;
+assign ack0 = ~ack;
+assign ack1 = ack;
+
 // Regs
-always @(posedge axis_aclk or negedge axis_aresetn) if (~axis_aresetn) req1 <= 1'b0; else req1 <= ~ack0 | req0;
-always @(posedge axis_aclk) if (en0) dat0 <= dat0_nxt;
+assign en = treq & tack;
+assign tack = ~ireq | iack;
+always @(posedge axis_aclk or negedge axis_aresetn) if (~axis_aresetn) ireq <= 1'b0; else ireq <= ~tack | treq;
+always @(posedge axis_aclk or negedge axis_aresetn) if (~axis_aresetn) ilast <= 1'b0; else if (en) ilast <= slast; else ilast <= 1'b0;
+always @(posedge axis_aclk) if (en) idata <= tdata;
 
 // Initiators
-assign ack1 = m0k_axis_tready;
-assign m0k_axis_tvalid = req1;
-assign m0k_axis_tdata = dat0;
-
+assign iack = m0k_axis_tready;
+assign m0k_axis_tvalid = ireq;
+assign m0k_axis_tdata = idata;
+assign m0k_axis_tlast = ilast;
 
 endmodule
 `default_nettype wire
